93c0b3e6caf299af560591e5bffb8bebcda47f5d
NotAlexNoyle <alexnoyle@icloud.com>
2024-12-30 08:05:57 -0500

    Fix build with TrueOG Bootstrap

---
+++ copyjar.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+cp fast-crystals-plugin-*/build/libs/fast-crystals-plugin-*.jar build/libs/FastCrystals-OG-1.0-SNAPSHOT.jar
+
+mv build/libs/FastCrystals-OG-1.0-SNAPSHOT.jar build/libs/FastCrystals-OG-1.0.jar
